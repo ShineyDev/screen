a8ab0f49d2c61a0edc34407488c32f1fd781bebd
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-02-22 10:23:55 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+import re
+import setuptools
+
+
+with open("screen/__init__.py", "r") as stream:
+    version = re.search(r"^version = [\"]([^\"]*)[\"]", stream.read(), re.MULTILINE).group(1)
+
+if version.endswith(("a", "b", "rc")):
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+classifiers = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Typing :: Typed",
+]
+
+project_urls = {
+    "Issue Tracker": "https://github.com/ShineyDev/screen/issues/",
+    "Source": "https://github.com/ShineyDev/screen/",
+}
+
+setuptools.setup(
+    author="ShineyDev",
+    classifiers=classifiers,
+    description="A Python library for creating TUI inspired by WPF.",
+    license="Apache Software License",
+    name="screen",
+    packages=["screen", "screen.drawing"],
+    project_urls=project_urls,
+    url="https://github.com/ShineyDev/screen",
+    version=version,
+)
