aa55d2c5a55bf761582630054d9701a9b97842dd
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-02-26 06:52:05 +0000

    version bump

--- screen/__init__.py
+++ screen/__init__.py
@@ -12,5 +12,5 @@ __all__ = [
 
 _VersionInfo = collections.namedtuple("_VersionInfo", "major minor patch release serial")
 
-version = "0.1.0a"
-version_info = _VersionInfo(0, 1, 0, "alpha", 0)
+version = "0.2.0a"
+version_info = _VersionInfo(0, 2, 0, "alpha", 0)
