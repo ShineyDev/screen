1a25c2cbf70cb2dbe89cd40705f6cf3e3736eb15
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-04-05 06:31:35 +0100

    use namedtuple._replace

--- screen/controls/__init__.py
+++ screen/controls/__init__.py
@@ -112,12 +112,12 @@ class ControlMeta(abc.ABCMeta):
 
                 if not p.doc:
                     if p.type is bool:
-                        p = _property(
-                            *p[:-1], f"Whether the {cls_name.lower()} {p.name.replace('_', ' ')}."
+                        p = p._replace(
+                            doc=f"Whether the {cls_name.lower()} {p.name.replace('_', ' ')}."
                         )
                     else:
-                        p = _property(
-                            *p[:-1], f"The {cls_name.lower()}'s {p.name.replace('_', ' ')}."
+                        p = p._replace(
+                            doc=f"The {cls_name.lower()}'s {p.name.replace('_', ' ')}."
                         )
 
                 properties.append(p)
