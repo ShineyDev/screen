a5b771b6fbac238f4039a714edf34cf5116d5ba9
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-04-06 21:21:59 +0100

    patch Stack stub

--- screen/controls/stack.pyi
+++ screen/controls/stack.pyi
@@ -1,11 +1,12 @@
 from typing import ClassVar, List, Optional, Union
 
 from screen.controls import Control
-from screen.controls.primitives import Bullet
+from screen.controls.primitives import Bullet, Orientation
 
 
 class Stack(Control):
     default_bullet: ClassVar[Union[Bullet, str]]
+    default_orientation: ClassVar[Orientation]
     default_spacing: ClassVar[int]
 
     def __init__(
@@ -13,6 +14,7 @@ class Stack(Control):
         *,
         children: List[Control],
         bullet: Union[Bullet, str]=...,
+        orientation: Orientation=...,
         spacing: int=...,
         **kwargs,
     ) -> None: ...
@@ -25,6 +27,10 @@ class Stack(Control):
     def children(self) -> List[Control]: ...
     @children.setter
     def children(self, value: List[Control]) -> None: ...
+    @property
+    def orientation(self) -> Orientation: ...
+    @orientation.setter
+    def orientation(self, value: Optional[Orientation]) -> None: ...
     @property
     def spacing(self) -> int: ...
     @spacing.setter
