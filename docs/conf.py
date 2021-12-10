f93b4bdf9d23133d5f6daa823b8b6ed2460eaa6f
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-12-10 06:48:03 +0000

    update documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,38 @@ import os
 import re
 import sys
 
+
 sys.path.insert(0, os.path.abspath(".."))
+
+
+author = "ShineyDev"
+project = "screen"
+
+copyright = f"2021-present, {author}"
+
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("../screen/__init__.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+release = "v" + match.group(2)
+version = release
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            release += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            release += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
 
 
 extensions = [
@@ -18,12 +49,19 @@ autodoc_member_order = "groupwise"
 autodoc_typehints = "none"
 
 extlinks = {
-    "issue": ("https://github.com/ShineyDev/screen/issues/%s", "#"),
+    "issue": (f"https://github.com/{author}/{project}/issues/%s", "#"),
 }
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
 }
+
+
+highlight_language = "none"
+pygments_style = "friendly"
+root_doc = "index"
+source_suffix = ".rst"
+
 
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
@@ -33,34 +71,4 @@ html_theme_options = {
     "prev_next_buttons_location": None,
     "titles_only": True,
 }
-master_doc = "index"
-pygments_style = "friendly"
-source_suffix = ".rst"
-
-copyright = "2021-present, ShineyDev"
-project = "screen"
-
-_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
-
-with open("../screen/__init__.py") as stream:
-    match = re.search(_version_regex, stream.read(), re.MULTILINE)
-
-version = match.group(2)
-
-if match.group(3) is not None:
-    try:
-        import subprocess
-
-        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += out.decode("utf-8").strip()
-
-        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += "+g" + out.decode("utf-8").strip()
-    except (Exception) as e:
-        pass
-
-release = version
+html_title = f"{project} {version}"
